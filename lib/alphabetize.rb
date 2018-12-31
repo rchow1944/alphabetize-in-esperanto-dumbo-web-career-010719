02cf76eb5bbad6896c78ba73662741905f5f316c
Raymond Chow <raychow1944@yahoo.com>
2018-12-31 09:33:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,8 @@
 def alphabetize(arr)
   # code here
-  sorted = arr.sort_by do |word|
+  arr.sort_by do |word|
     word.each_char do |ch|
       "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars.index(ch)
     end
   end
-  sorted
 end
