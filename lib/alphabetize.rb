fc97ae2f04d55252d28294c2ae17f8b10a23b2f0
Raymond Chow <raychow1944@yahoo.com>
2018-12-31 09:37:27 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,8 @@ def alphabetize(arr)
   # code here
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
-    word.each_char do |ch|
+    word.each_char {ch|
       esperanto.chars.index(ch)
-    end
+    }
   end
 end
